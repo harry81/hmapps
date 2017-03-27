83e796aec1cea72b1be2949b554b3a18ff2bb937
Hyunmin Choi <pointer81@gmail.com>
2017-03-27 18:45:01 +0900

    WIP parse hani

--- water/management/commands/fetcharticles.py
+++ water/management/commands/fetcharticles.py
@@ -1,13 +1,66 @@
-from django.core.management.base import BaseCommand
+# -*- coding: utf-8 -*-
+import sys
 import requests
+from urlparse import urlparse
+from django.core.management.base import BaseCommand
+from django.core.files.storage import default_storage
+from lxml import etree
+from datetime import datetime
+import feedparser
+from StringIO import StringIO
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 class Command(BaseCommand):
     help = "My shiny new management command."
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('sample', nargs='+')
+    def fetch_news_to_S3(self):
+        feed = feedparser.parse('http://www.hani.co.kr/rss/')
+
+        for ele in feed.entries:
+            url = ele.link
+            category = 'hani'
+            rep = requests.get(url)
+
+            filename = urlparse(url).path.split('/')[-1]
+            import ipdb; ipdb.set_trace()
+            fp = default_storage.open('%s/%s' % (category, filename), 'w')
+            fp.write(rep.content)
+            fp.close()
+
+            print "Fetched %s, Save on S3 %s" % (rep.ok, fp)
+
+    def load_from_S3(self):
+        for ele in default_storage.bucket.list():
+            print ele
+            content = ele.read()
+            root = etree.HTML(content)
+
+            try:
+                url = root.xpath('//meta[@property="og:url"]/@content')[0]
+                title = root.xpath('//title/text()')[0]
+
+                subtitle = root.xpath('//div[@class="subtitle"]/text()')
+                subtitle = '\n'.join(map(lambda x: x.strip(), subtitle))
+
+                text = root.xpath('//div[@class="text"]/text()')
+                text = '\n'.join(map(lambda x: x.strip(), text))
+
+                print url, title, subtitle, text
+
+                try:
+                    publish_at =  root.xpath('//p[@class="date-time"]/span/text()')
+
+                except ValueError as e:
+                    publish_at =  root.xpath('//p[@class="date"]/span/text()')
+
+
+            except IndexError as e:
+                print e
+
 
     def handle(self, *args, **options):
-        url = "http://www.hani.co.kr/arti/economy/economy_general/787918.html"
-        rep = requests.get(url)
-        print rep.ok
+        self.load_from_S3()
+        # self.fetch_news_to_S3()
