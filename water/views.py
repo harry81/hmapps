f1125fa58493af54599f146e13fed70209349fd4
Hyunmin Choi <pointer81@gmail.com>
2017-04-21 19:43:12 +0900

    Show contents

--- water/views.py
+++ water/views.py
@@ -3,11 +3,13 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from .models import Item
 # Create your views here.
 
 
 def index(request):
+    items = Item.objects.all()
     return render(request, 'water/list.html', {
-        'poll': 'hi',
+        'items': items,
         'error_message': "You didn't select a choice.",
     })
