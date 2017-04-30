21c66ac5cc30b9140d99574c883aff0335fecc8e
Hyunmin Choi <pointer81@gmail.com>
2017-04-30 20:54:58 +0900

    Apply bootstrap style

--- water/utils.py
+++ water/utils.py
@@ -99,7 +99,6 @@ def _get_prefix(url):
 
 def insert_news_to_db(articles):
     for article in articles:
-        print article['publish_at']
         Item.objects.update_or_create(url=article['url'], defaults=article)
 
 
