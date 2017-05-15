460d614e6d96317ed5a1a5d6158ab68111aaf43e
Hyunmin Choi <pointer81@gmail.com>
2017-05-15 18:29:27 +0900

    Item list can be filtered at admin page

--- water/admin.py
+++ water/admin.py
@@ -3,8 +3,8 @@ from .models import Item
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ("title", "publisher",
-                    "publish_at")
+    list_display = ("title", "publisher", "publish_at")
+    list_filter = ("publisher", "publish_at")
 
 
 admin.site.register(Item, ItemAdmin)
