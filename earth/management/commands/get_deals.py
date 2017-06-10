053eb86a01b1163b56d13087fa3316f9cc571359
Hyunmin Choi <pointer81@gmail.com>
2017-06-10 14:04:11 +0900

    Respect url argument

--- earth/management/commands/get_deals.py
+++ earth/management/commands/get_deals.py
@@ -4,12 +4,21 @@ import requests
 from django.core.management.base import BaseCommand
 from earth.utils import rename_fields
 
+url = 'http://localhost:8001'
+
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--url', nargs='+')
+
     def handle(self, *args, **options):
+
+        if options['url']:
+            url = options['url'][0]
+
         deals_as_string = open('list_47190_201701.xml', 'r').read()
         deals = xmltodict.parse(deals_as_string)
 
         for item in deals['response']['body']['items']['item']:
             item = rename_fields(item)
-            res = requests.post('http://localhost:8001/en/api/earth/deal/', data=item)
+            res = requests.post("%s/en/api/earth/deal/" % url, data=item)
